1f6f9a246e14cd7ca21872e9c96f53a810938897
james-muriithi <muriithijames556@gmail.com>
2021-09-16 15:47:25 +0300

    dark theme toggle

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,39 @@
 <template>
   <v-container>
+    <div>
+      <div class="purple darken-2 text-center">
+    <span class="white--text">Lorem ipsum</span>
+  </div>
+      <v-tooltip v-if="!$vuetify.theme.dark" bottom data-app>
+        <template v-slot:activator="{ on }">
+          <v-btn v-on="on" color="primary" small fab @click="darkMode">
+            <v-icon class="mr-1">mdi-moon-waxing-crescent</v-icon>
+          </v-btn>
+        </template>
+        <span class="primary--text">Dark Mode On</span>
+      </v-tooltip>
+
+      <v-tooltip v-else bottom data-app>
+        <template v-slot:activator="{ on }">
+          <v-btn v-on="on" color="secondary" small fab @click="darkMode">
+            <v-icon color="yellow">mdi-white-balance-sunny</v-icon>
+          </v-btn>
+        </template>
+        <span>Dark Mode Off</span>
+      </v-tooltip>
+    </div>
   </v-container>
 </template>
 
 <script>
-  export default {
-    name: 'HelloWorld',
+export default {
+  name: "HelloWorld",
 
-    data: () => ({
-    }),
-  }
+  data: () => ({}),
+  methods: {
+    darkMode() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    },
+  },
+};
 </script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,30 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: colors.purple,
+                secondary: colors.grey.darken1,
+                accent: colors.shades.black,
+                error: colors.red.accent3,
+                background: colors.indigo.lighten5
+            },
+            dark: {
+                primary: colors.blue.lighten3,
+                background: colors.indigo.base
+            },
+
+        },
+        options: {
+            themeCache: {
+                get: key => localStorage.getItem(key),
+                set: (key, value) => localStorage.setItem(key, value),
+            },
+        },
+    }
 });
