ab2bbd7a700aaa32a5050bb376360192b6ddc411
james-muriithi <muriithijames556@gmail.com>
2021-09-19 23:48:35 +0300

    save browse data to local storage

--- src/store/modules/music/actions.js
+++ src/store/modules/music/actions.js
@@ -1,9 +1,29 @@
-import { extractAlbums, extractPlaylists, extractSongs } from '../../../helpers';
+import { extractAlbums, extractPlaylists, extractSongs, getFromLocalStorage, saveToLocalStorage } from '../../../helpers';
 import { fetchBrowseData } from '../../../helpers/apple-music';
 
 export default {
     async fetchBrowseData({ commit }, { limit = 20 } = {}) {
-        const data = await fetchBrowseData(limit);
+
+        let savedData = getFromLocalStorage('browse-data');
+
+        try {
+            savedData = JSON.parse(savedData);
+        } catch (error) {
+            console.log(error);
+        }
+
+        let data;
+
+        if (savedData && savedData.expiry_date > 1632087860490) {
+            data = savedData;
+        } else {
+            data = await fetchBrowseData(limit);
+            data.expiry_date = Date.now() + 3600000;
+            saveToLocalStorage('browse-data', JSON.stringify(data));
+        }
+
+
+
         const songs = extractSongs(data.songs[0].data);
         const albums = extractAlbums(data.albums[0].data, 12);
         const playlists = extractPlaylists(data.playlists[0].data, 4);
