32733ea2924c97766c1d57314855c938272e69fb
james-muriithi <muriithijames556@gmail.com>
2021-10-17 14:42:49 +0300

    added search input

--- src/components/UI/AppBar.vue
+++ src/components/UI/AppBar.vue
@@ -5,21 +5,28 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn icon>
+      <v-text-field
+        dense
+        single-line
+        prepend-inner-icon="mdi-magnify"
+        outlined
+      ></v-text-field>
+
+      <!-- <v-btn icon>
         <v-icon>mdi-magnify</v-icon>
-      </v-btn>
+      </v-btn> -->
       <theme-toggle class="ml-2 ml-sm-0" />
     </v-app-bar>
   </v-card>
 </template>
 
 <script>
-import ThemeToggle from './ThemeToggle.vue';
+import ThemeToggle from "./ThemeToggle.vue";
 
 export default {
   name: "AppBar",
   components: {
-    ThemeToggle
+    ThemeToggle,
   },
   methods: {
     toggleDrawer() {
@@ -33,7 +40,24 @@ export default {
 .app-bar {
   background: var(--v-background-base) !important;
 }
-.card{
-    margin-bottom: 8px;
+.card {
+  margin-bottom: 8px;
+}
+</style>
+
+<style lang="scss">
+.app-bar {
+  .v-text-field__details {
+    display: none !important;
+    margin: 0 !important;
+  }
+  .v-text-field{
+    max-width: 200px !important;
+    margin-right: 10px !important;
+    @media screen and (min-width: 992px) {
+      max-width: 300px !important;
+      margin-right: 20px !important;
+    }
+  }
 }
 </style>
