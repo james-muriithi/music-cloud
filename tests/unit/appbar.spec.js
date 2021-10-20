563db4a073dad2b4e07ebe1ace43bc543a58e5c6
james-muriithi <muriithijames556@gmail.com>
2021-10-20 12:17:46 +0300

    edited appbar test

--- tests/unit/appbar.spec.js
+++ tests/unit/appbar.spec.js
@@ -1,16 +1,12 @@
 import { shallowMount } from "@vue/test-utils";
 import AppBar from "@/components/UI/AppBar.vue";
-
-const $route = {
-  name: "home",
-};
+import VueRouter from "vue-router";
 
 describe("AppBar.vue", () => {
   it("renders", () => {
+    const router = new VueRouter();
     const wrapper = shallowMount(AppBar, {
-      mocks: {
-        $route,
-      },
+      router
     });
 
     expect(wrapper.exists()).toBe(true);
--- tests/unit/index.js
+++ tests/unit/index.js
@@ -1,4 +1,7 @@
 import Vue from "vue";
 import Vuetify from "vuetify";
+import VueRouter from "vue-router";
+
 Vue.config.productionTip = false;
 Vue.use(Vuetify);
+Vue.use(VueRouter)
