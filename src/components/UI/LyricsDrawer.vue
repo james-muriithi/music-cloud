13a4eef911b5d4152d0bddc8c1833852ba8e6865
james-muriithi <muriithijames556@gmail.com>
2021-10-25 11:24:24 +0300

    lyrics nav drawer

---
+++ src/components/UI/LyricsDrawer.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+    :width="400"
+    :permanent="drawer"
+    fixed
+    right
+  >
+    <v-container>
+      <v-btn icon @click="closeDrawer">
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+    </v-container>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: "LyricsDrawer",
+  data() {
+    return {
+      drawer: true,
+    };
+  },
+  methods: {
+    closeDrawer() {
+      this.drawer = false;
+    },
+  },
+};
+</script>
---
+++ src/components/player/controls/Lyrics.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-btn icon :style="cssProps" :title="title">
+    <v-icon>{{ icon }}</v-icon>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: "Lyrics",
+  props: {
+    size: {
+      type: Number,
+      default: 18,
+    },
+    icon: {
+      type: String,
+      default: "mdi-file-music-outline",
+    },
+  },
+  computed: {
+    cssProps() {
+      return {
+        "--size": `${this.size}px`,
+      };
+    },
+    title() {
+      return "Lyrics";
+    },
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+button {
+  i.v-icon {
+    font-size: var(--size) !important;
+  }
+}
+</style>
