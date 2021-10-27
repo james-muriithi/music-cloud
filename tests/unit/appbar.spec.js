c0baf4f53a1c39992e7e8e571adb30029c286a8a
james-muriithi <muriithijames556@gmail.com>
2021-10-27 23:47:40 +0300

    added github workflows and fixed lints

--- tests/unit/appbar.spec.js
+++ tests/unit/appbar.spec.js
@@ -1,13 +1,11 @@
 import { shallowMount } from "@vue/test-utils";
 import AppBar from "@/components/UI/AppBar.vue";
-import VueRouter from "vue-router";
 import router from '@/router'
 
 describe("AppBar.vue", () => {
   let wrapper;
 
   beforeEach(() => {
-    // const router = new VueRouter();
     wrapper = shallowMount(AppBar, {
       router,
     });
