f0240eb61191227eb9b0e6e01c444cfcb8bc96c2
james-muriithi <muriithijames556@gmail.com>
2021-11-01 21:47:34 +0300

    formatted mixin

--- src/mixin.js
+++ src/mixin.js
@@ -1,13 +1,13 @@
 export default {
-    methods: {
-        msToTime(duration) {
-            let seconds = Math.floor((duration / 1000) % 60),
-                minutes = Math.floor((duration / (1000 * 60)) % 60);
+  methods: {
+    msToTime(duration) {
+      let seconds = Math.floor((duration / 1000) % 60),
+        minutes = Math.floor((duration / (1000 * 60)) % 60);
 
-            minutes = (minutes < 10 && minutes > 0) ? "0" + minutes : minutes;
-            seconds = (seconds < 10) ? "0" + seconds : seconds;
+      minutes = minutes < 10 && minutes > 0 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
 
-            return minutes + ":" + seconds;
-        },
-    }
+      return minutes + ":" + seconds;
+    },
+  },
 };
